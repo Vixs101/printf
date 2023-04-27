601024ffdfb8c58a3a9b197f6aa569c79be3b002
michelle <omekemichelle@gmail.com>
2023-04-27 05:53:48 -0700

    main.h

--- main.h
+++ main.h
@@ -3,5 +3,10 @@
 
 #include <stdio.h>
 
+int printchar(va_list args);
+int printstring(va_list args);
+int printpercent(va_list args);
+int print_int(va_list args);
+int _printf(const char *format, ...);
 
 #endif /* MAIN_H*/
